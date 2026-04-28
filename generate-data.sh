00f1934f81b5b1144b663b203e57741e7280ee33
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2026-04-28 02:02:30 +0000

    1200 reqs/s

--- generate-data.sh
+++ generate-data.sh
@@ -12,7 +12,7 @@ set -euo pipefail
 
 time ./data-generator/generate \
     --reuse-refs \
-    --payloads 60100 \
+    --payloads 72100 \
     --payloads-out test/test-data.json \
     --fraud-ratio-payloads 0.47 \
     --mcc-cfg resources/mcc_risk.json
